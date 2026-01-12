97090783b5861f91e8087a7092a999e5eae970ee
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-01-12 22:16:02 +0700

    refactor(api/note): I wrote myself the list and post

--- .nvim.lua
+++
@@ -1,7 +0,0 @@
-vim.filetype.add({
-  pattern = {
-    ["api/edit/.*/.*%.yaml"] = "yaml.openapi",
-    ["api/note/.*/.*%.yaml"] = "yaml.openapi",
-    ["api/shared/.*/.*%.yaml"] = "yaml.openapi",
-  },
-})
