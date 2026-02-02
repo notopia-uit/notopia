bd365fa84ad21dc0bae939d1fc34450c3064cdfd
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-02-02 17:09:42 +0700

    chore: update tailwind postcss, and setup buf

--- .nvim.lua
+++ .nvim.lua
@@ -1,25 +1,25 @@
-vim.lsp.config("yamlls", {
-  ---@module 'codesettings'
-  ---@type lsp.yamlls
-  settings = {
-    yaml = {
-      customTags = {
-        "!Condition sequence",
-        "!Context scalar",
-        "!Enumerate sequence",
-        "!Env scalar",
-        "!File scalar",
-        "!File sequence",
-        "!Find sequence",
-        "!Format sequence",
-        "!If sequence",
-        "!Index scalar",
-        "!KeyOf scalar",
-        "!Value scalar",
-        "!AtIndex scalar",
-      },
-    },
-  },
-})
+-- vim.lsp.config("yamlls", {
+--   ---@module 'codesettings'
+--   ---@type lsp.yamlls
+--   settings = {
+--     yaml = {
+--       customTags = {
+--         "!Condition sequence",
+--         "!Context scalar",
+--         "!Enumerate sequence",
+--         "!Env scalar",
+--         "!File scalar",
+--         "!File sequence",
+--         "!Find sequence",
+--         "!Format sequence",
+--         "!If sequence",
+--         "!Index scalar",
+--         "!KeyOf scalar",
+--         "!Value scalar",
+--         "!AtIndex scalar",
+--       },
+--     },
+--   },
+-- })
 
 vim.o.backupcopy = "yes" -- https://github.com/nrwl/nx/issues/20622
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -12,9 +12,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './{src,pages,components,app}/**/*.{ts,tsx,js,jsx,html}',
-    '!./{src,pages,components,app}/**/*.{stories,spec}.{ts,tsx,js,jsx,html}',
-//     ...createGlobPatternsForDependencies(__dirname)
+    "./{src,pages,components,app}/**/*.{ts,tsx,js,jsx,html}",
+    "!./{src,pages,components,app}/**/*.{stories,spec}.{ts,tsx,js,jsx,html}",
+    //     ...createGlobPatternsForDependencies(__dirname)
   ],
   theme: {
     extend: {},
