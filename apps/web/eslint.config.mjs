0b492d1ce3010f3aeab3d30801c1e8de390def57
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-02-03 08:13:43 +0700

    feat: eslint tailwind, eslint react hook, renovate compose
    Closed: #19
    Closed: #22

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,12 +1,27 @@
+import { defineConfig } from "eslint/config";
+import baseConfig from "../../eslint.config.mjs";
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
 import nextEslintPluginNext from "@next/eslint-plugin-next";
 import nx from "@nx/eslint-plugin";
-import baseConfig from "../../eslint.config.mjs";
-import { defineConfig } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default defineConfig(
-  { plugins: { "@next/next": nextEslintPluginNext } },
   ...baseConfig,
+  ...reactHooks.configs.flat.recommended,
+  {
+    plugins: {
+      "@next/next": nextEslintPluginNext,
+    },
+  },
   ...nx.configs["flat/react-typescript"],
+  {
+    extends: [eslintPluginBetterTailwindcss.configs.recommended],
+    settings: {
+      "better-tailwindcss": {
+        entryPoint: "./src/app/global.css",
+      },
+    },
+  },
   {
     ignores: [".next/**/*", "**/out-tsc"],
   },
