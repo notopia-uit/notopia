a60f2287206b11d3b397dc300e89805605847513
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-02-09 00:15:36 +0700

    feat: otel for pgx

--- go.mod
+++ go.mod
@@ -39,6 +39,7 @@ require (
 	github.com/cenkalti/backoff/v5 v5.0.3 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
+	github.com/exaring/otelpgx v0.10.0 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
--- pkg/note/infra/persistence/pg/pg.go
+++ pkg/note/infra/persistence/pg/pg.go
@@ -3,13 +3,26 @@ package pg
 import (
 	"context"
 
+	"github.com/exaring/otelpgx"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/notopia-uit/notopia/pkg/common/config"
 	"github.com/notopia-uit/notopia/pkg/note/infra/persistence/pgsqlc"
+	"go.opentelemetry.io/otel/sdk/trace"
 )
 
-func NewDBTXPool(ctx context.Context, cfg *config.SQL) (pgsqlc.DBTX, error) {
-	pool, err := pgxpool.New(ctx, cfg.URL)
+func NewDBTXPool(
+	ctx context.Context,
+	tracerProvider *trace.TracerProvider,
+	cfg *config.SQL,
+) (pgsqlc.DBTX, error) {
+	pgxCfg, err := pgxpool.ParseConfig(cfg.URL)
+	if err != nil {
+		return nil, err
+	}
+	pgxCfg.ConnConfig.Tracer = otelpgx.NewTracer(
+		otelpgx.WithTracerProvider(tracerProvider),
+	)
+	pool, err := pgxpool.NewWithConfig(ctx, pgxCfg)
 	if err != nil {
 		return nil, err
 	}
