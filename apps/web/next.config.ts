dcccba57ca5b2bd1309d36262498e9919535c452
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-03-05 19:52:58 +0700

    fix: tired

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: WithNxOptions = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  output: 'standalone',
 };
 
 const plugins = [
