6e720015c6a9f7d5cb0a10a562b80a8550eeb14f
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-03-09 23:55:39 +0700

    fix(docs): vitepress doesn't has mermaid builtin, what a shame

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,14 +1,28 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue';
+import { h, nextTick, watch } from 'vue';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import '@catppuccin/vitepress/theme/mocha/lavender.css';
 import { theme } from 'vitepress-openapi/client';
 import 'vitepress-openapi/dist/style.css';
+import { createMermaidRenderer } from 'vitepress-mermaid-renderer';
+import { useData } from 'vitepress';
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
+    const { isDark } = useData();
+    const initMermaid = () => {
+      const mermaidRenderer = createMermaidRenderer({
+        theme: isDark.value ? 'dark' : 'forest',
+      });
+      mermaidRenderer.setToolbar({
+        showLanguageLabel: false,
+      });
+    };
+    nextTick(initMermaid);
+    watch(() => isDark.value, initMermaid);
+
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     });
