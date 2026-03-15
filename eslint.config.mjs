dbd78e6e3a0649d45efc8b4ecb0e31d06c2fb1e4
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-03-15 21:45:16 +0700

    chore: eslint no warn _

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,7 +21,7 @@ export default defineConfig(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/no-unused-vars': [
-        'warn',
+        'off',
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
