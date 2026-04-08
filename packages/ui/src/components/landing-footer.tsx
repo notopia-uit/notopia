37909e9244d345973dd8bd77fc856a17ce7b02d3
NTGNguyen <ntgnguyen@duck.com>
2026-04-08 13:42:22 +0700

    feat(ui): add landing page

---
+++ packages/ui/src/components/landing-footer.tsx
@@ -0,0 +1,30 @@
+import { ModeToggle } from './theme-mode-toggle';
+import { Separator } from '@notopia-uit/ui/components/ui/separator';
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container border-t flex flex-col md:flex-row items-center justify-between py-2 px-16 space-y-4">
+        <div>
+          <div className="space-y-1">
+            <h4 className="text-sm leading-none font-medium">Note Land</h4>
+            <p className="text-muted-foreground text-sm">
+              An open-source Note for user.
+            </p>
+          </div>
+          <Separator className="my-4" />
+          <div className="flex h-5 items-center space-x-4 text-sm">
+            <div>Blog</div>
+            <Separator orientation="vertical" />
+            <div>Docs</div>
+            <Separator orientation="vertical" />
+            <div>Source</div>
+          </div>
+        </div>
+        <div>
+          <ModeToggle />
+        </div>
+      </div>
+    </footer>
+  );
+}
