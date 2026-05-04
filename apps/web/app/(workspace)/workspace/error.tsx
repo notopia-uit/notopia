dc99c817148c5dcd3e2534f2fcb06fd5316bfb7c
NTGNguyen <ntgnguyen@duck.com>
2026-05-04 23:29:29 +0700

    fix: it should is client components

--- apps/web/app/(workspace)/workspace/error.tsx
+++ apps/web/app/(workspace)/workspace/error.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@ui/components/shadcn/button';
-import { ArrowLeft } from 'lucide-react';
+'use client'
+import ErrorBlock from '@notopia-uit/ui/components/error-button';
 
 const ErrorPage = () => {
   return (
@@ -9,24 +9,7 @@ const ErrorPage = () => {
       xl:grid-cols-2
     "
     >
-      <div className="flex flex-col p-16">
-        <div
-          className="
-          mt-8 flex flex-1 flex-col items-center justify-center text-center
-          xl:items-start xl:text-start
-        "
-        >
-          <div className="mb-3 flex items-center gap-3">
-            <span className="text-sm font-semibold">404</span>
-          </div>
-          <h1 className="mb-2 text-4xl font-bold">Page Not Found</h1>
-          <p>Oops! The page you're trying to access doesn't exist.</p>
-          <Button className="mt-8 h-9 cursor-pointer px-4 py-2">
-            <ArrowLeft />
-            <span>Go Back Home</span>
-          </Button>
-        </div>
-      </div>
+      <ErrorBlock />
       <div
         className="
         relative hidden
