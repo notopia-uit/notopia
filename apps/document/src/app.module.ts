f5b8f9dde90418624dcb3e515d89b093c27ab844
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-05-11 16:36:57 +0700

    fix: kafka module globally

--- apps/document/src/app.module.ts
+++ apps/document/src/app.module.ts
@@ -1,14 +1,13 @@
 import { Inject, Module, OnModuleInit } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
-import { ClientKafka, ClientsModule } from '@nestjs/microservices';
+import { ClientKafka } from '@nestjs/microservices';
 import { ApiModule } from '@notopia-uit/api-document-nestjs-server';
 import { OpenTelemetryModule } from 'nestjs-otel';
 import { LoggerModule } from 'nestjs-pino';
 import pretty from 'pino-pretty';
 
 import { AuthenticationModule } from '#/authentication/authentication.module';
-import { getKafkaConfig } from '#/config/kafka.config';
 import { HocuspocusModule } from '#/hocuspocus/hocuspocus.module';
 
 import { AuthorizationModule } from './authorization/authorization.module';
@@ -28,6 +27,7 @@ import {
 import { DatabaseModule } from './database/database.module';
 import { DocumentApi } from './document/document.api';
 import { DocumentModule } from './document/document.module';
+import { KafkaModule } from './kafka/kafka.module';
 import { NoteModule } from './note/note.module';
 import { RevisionApi } from './revision/revision.api';
 import { RevisionModule } from './revision/revision.module';
@@ -70,14 +70,9 @@ import { StorageModule } from './storage/storage.module';
         };
       },
     }),
-    ClientsModule.registerAsync([
-      {
-        name: KAFKA_CLIENT,
-        useFactory: getKafkaConfig,
-        inject: [ConfigService],
-      },
-    ]),
+
     StorageModule,
+    KafkaModule,
     AuthenticationModule,
     AuthorizationModule,
     NoteModule,
--- apps/document/src/kafka/kafka.module.ts
+++ apps/document/src/kafka/kafka.module.ts
@@ -1,18 +1,19 @@
 import { Global, Module } from '@nestjs/common';
-import { ClientsModule } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { ClientProxyFactory } from '@nestjs/microservices';
 
 import { KAFKA_CLIENT } from '#/common/token';
 import { getKafkaConfig } from '#/config/kafka.config';
 
 @Global()
 @Module({
-  imports: [
-    ClientsModule.registerAsync([
-      {
-        name: KAFKA_CLIENT,
-        useFactory: getKafkaConfig,
-      },
-    ]),
+  providers: [
+    {
+      provide: KAFKA_CLIENT,
+      useFactory: (configService: ConfigService) =>
+        ClientProxyFactory.create(getKafkaConfig(configService)),
+      inject: [ConfigService],
+    },
   ],
   exports: [KAFKA_CLIENT],
 })
