c8e323cdfb2a04ac8bb242930b3cfed89d5106a8
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-05-19 13:51:56 +0700

    fix: remove some local default env for those js services

--- apps/document/src/config/config.factory.ts
+++ apps/document/src/config/config.factory.ts
@@ -1,11 +1,17 @@
 import { registerAs } from '@nestjs/config';
 
 import {
+  AppConfig,
   appConfigSchema,
+  AuthenticationConfig,
   authenticationConfigSchema,
+  DatabaseConfig,
   databaseConfigSchema,
+  KafkaConfig,
   kafkaConfigSchema,
+  S3Config,
   s3ConfigSchema,
+  ServicesConfig,
   servicesConfigSchema,
 } from './config';
 
@@ -13,23 +19,24 @@ export const APP_CONFIG = Symbol('APP_CONFIG');
 
 export const appConfig = registerAs(APP_CONFIG, () =>
   appConfigSchema.parse({
-    env: process.env.NODE_ENV ?? 'production',
-    logLevel: process.env.NOTOPIA_DOCUMENT_LOG_LEVEL ?? 'warn',
+    env: (process.env.NODE_ENV as AppConfig['env'] | undefined) ?? 'production',
+    logLevel:
+      (process.env.NOTOPIA_DOCUMENT_LOG_LEVEL as AppConfig['logLevel'] | undefined) ?? 'warn',
     port: parseInt(process.env.NOTOPIA_DOCUMENT_PORT ?? '8082', 10),
-    apiUrl: process.env.NOTOPIA_DOCUMENT_API_URL ?? '',
-  })
+    apiUrl: process.env.NOTOPIA_DOCUMENT_API_URL!,
+  } satisfies AppConfig)
 );
 
 export const DATABASE_CONFIG = Symbol('DATABASE_CONFIG');
 
 export const getDatabaseConfig = () =>
   databaseConfigSchema.parse({
-    host: process.env.NOTOPIA_DOCUMENT_DB_HOST ?? 'localhost',
-    port: parseInt(process.env.NOTOPIA_DOCUMENT_DB_PORT ?? '5434', 10),
-    username: process.env.NOTOPIA_DOCUMENT_DB_USER ?? 'postgres',
-    password: process.env.NOTOPIA_DOCUMENT_DB_PASSWORD ?? '',
+    host: process.env.NOTOPIA_DOCUMENT_DB_HOST!,
+    port: parseInt(process.env.NOTOPIA_DOCUMENT_DB_PORT!, 10),
+    username: process.env.NOTOPIA_DOCUMENT_DB_USER!,
+    password: process.env.NOTOPIA_DOCUMENT_DB_PASSWORD!,
     database: process.env.NOTOPIA_DOCUMENT_DB_NAME ?? 'document',
-  });
+  } satisfies DatabaseConfig);
 
 export const databaseConfig = registerAs(DATABASE_CONFIG, getDatabaseConfig);
 
@@ -37,21 +44,21 @@ export const SERVICE_CONFIG = Symbol('SERVICE_CONFIG');
 
 export const servicesConfig = registerAs(SERVICE_CONFIG, () =>
   servicesConfigSchema.parse({
-    noteUrl: process.env.NOTOPIA_DOCUMENT_SERVICES_NOTE_GRPC_URL ?? '',
-    authorizationUrl: process.env.NOTOPIA_DOCUMENT_SERVICES_AUTHORIZATION_GRPC_URL ?? '',
-  })
+    noteUrl: process.env.NOTOPIA_DOCUMENT_SERVICES_NOTE_GRPC_URL!,
+    authorizationUrl: process.env.NOTOPIA_DOCUMENT_SERVICES_AUTHORIZATION_GRPC_URL!,
+  } satisfies ServicesConfig)
 );
 
 export const S3_CONFIG = Symbol('S3_CONFIG');
 
 export const s3Config = registerAs(S3_CONFIG, () =>
   s3ConfigSchema.parse({
-    endpoint: process.env.NOTOPIA_DOCUMENT_S3_ENDPOINT ?? '',
+    endpoint: process.env.NOTOPIA_DOCUMENT_S3_ENDPOINT!,
     region: process.env.NOTOPIA_DOCUMENT_S3_REGION ?? 'us-east-1',
-    accessKeyId: process.env.NOTOPIA_DOCUMENT_S3_ACCESS_KEY_ID ?? '',
-    secretAccessKey: process.env.NOTOPIA_DOCUMENT_S3_SECRET_ACCESS_KEY ?? '',
-    bucketName: process.env.NOTOPIA_DOCUMENT_S3_BUCKET_NAME ?? 'document',
-  })
+    accessKeyId: process.env.NOTOPIA_DOCUMENT_S3_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.NOTOPIA_DOCUMENT_S3_SECRET_ACCESS_KEY!,
+    bucketName: process.env.NOTOPIA_DOCUMENT_S3_BUCKET_NAME!,
+  } satisfies S3Config)
 );
 
 export const KAFKA_CONFIG = Symbol('KAFKA_CONFIG');
@@ -59,27 +66,26 @@ export const KAFKA_CONFIG = Symbol('KAFKA_CONFIG');
 export const kafkaConfig = registerAs(KAFKA_CONFIG, () =>
   kafkaConfigSchema.parse({
     clientId: process.env.NOTOPIA_DOCUMENT_KAFKA_CLIENT_ID ?? 'document',
-    brokers: (process.env.NOTOPIA_DOCUMENT_KAFKA_BROKERS ?? 'localhost:19092')
-      .split(',')
+    brokers: process.env
+      .NOTOPIA_DOCUMENT_KAFKA_BROKERS!.split(',')
       .map((v) => v.trim())
       .filter(Boolean),
     groupId: process.env.NOTOPIA_DOCUMENT_KAFKA_GROUP_ID ?? 'document',
-  })
+  } satisfies KafkaConfig)
 );
 
 export const AUTHENTICATION_CONFIG = Symbol('AUTHENTICATION_CONFIG');
 
 export const authenticationConfig = registerAs(AUTHENTICATION_CONFIG, () =>
   authenticationConfigSchema.parse({
-    jwksUrls:
-      process.env.NOTOPIA_DOCUMENT_AUTHENTICATION_JWKS_URLS?.split(',')
-        .map((v) => v.trim())
-        .filter(Boolean) ?? [],
+    jwksUrls: process.env.NOTOPIA_DOCUMENT_AUTHENTICATION_JWKS_URLS?.split(',')
+      .map((v) => v.trim())
+      .filter(Boolean)!,
     issuers: process.env.NOTOPIA_DOCUMENT_AUTHENTICATION_ISSUERS?.split(',')
       .map((v) => v.trim())
       .filter(Boolean),
     audiences: process.env.NOTOPIA_DOCUMENT_AUTHENTICATION_AUDIENCES?.split(',')
       .map((v) => v.trim())
       .filter(Boolean),
-  })
+  } satisfies AuthenticationConfig)
 );
--- apps/search-worker/src/config.factory.ts
+++ apps/search-worker/src/config.factory.ts
@@ -1,8 +1,11 @@
 import { registerAs } from '@nestjs/config';
 
 import {
+  AppConfig,
   appConfigSchema,
+  KafkaConfig,
   kafkaConfigSchema,
+  MeiliConfig,
   meiliConfigSchema,
 } from './config';
 
@@ -10,10 +13,11 @@ export const APP_CONFIG = Symbol('APP_CONFIG');
 
 export const appConfig = registerAs(APP_CONFIG, () =>
   appConfigSchema.parse({
-    env: process.env.NODE_ENV ?? 'production',
-    logLevel: process.env.NOTOPIA_SEARCH_WORKER_LOG_LEVEL ?? 'warn',
+    env: (process.env.NODE_ENV as AppConfig['env'] | undefined) ?? 'production',
+    logLevel:
+      (process.env.NOTOPIA_SEARCH_WORKER_LOG_LEVEL as AppConfig['logLevel'] | undefined) ?? 'warn',
     port: parseInt(process.env.NOTOPIA_SEARCH_WORKER_PORT ?? '8084', 10),
-  })
+  } satisfies AppConfig)
 );
 
 export const KAFKA_CONFIG = Symbol('KAFKA_CONFIG');
@@ -21,19 +25,19 @@ export const KAFKA_CONFIG = Symbol('KAFKA_CONFIG');
 export const kafkaConfig = registerAs(KAFKA_CONFIG, () =>
   kafkaConfigSchema.parse({
     clientId: process.env.NOTOPIA_SEARCH_WORKER_KAFKA_CLIENT_ID ?? 'search-worker',
-    brokers: (process.env.NOTOPIA_SEARCH_WORKER_KAFKA_BROKERS ?? 'localhost:19092')
-      .split(',')
+    brokers: process.env
+      .NOTOPIA_SEARCH_WORKER_KAFKA_BROKERS!.split(',')
       .map((v) => v.trim())
       .filter(Boolean),
     groupId: process.env.NOTOPIA_SEARCH_WORKER_KAFKA_GROUP_ID ?? 'search-worker',
-  })
+  } satisfies KafkaConfig)
 );
 
 export const MEILI_CONFIG = Symbol('MEILI_CONFIG');
 
 export const meiliConfig = registerAs(MEILI_CONFIG, () =>
   meiliConfigSchema.parse({
-    host: process.env.NOTOPIA_SEARCH_WORKER_MEILI_HOST ?? '',
-    apiKey: process.env.NOTOPIA_SEARCH_WORKER_MEILI_API_KEY ?? '',
-  })
+    host: process.env.NOTOPIA_SEARCH_WORKER_MEILI_HOST!,
+    apiKey: process.env.NOTOPIA_SEARCH_WORKER_MEILI_API_KEY!,
+  } satisfies MeiliConfig)
 );
