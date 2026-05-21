fc699cb5584c6392e9a19f67ab3131c988d4a54e
NTGNguyen <ntgnguyen@duck.com>
2026-05-21 11:27:43 +0700

    fix: need to be awaited

--- apps/web/app/(workspace)/workspace/[workspaceId]/note/[noteId]/graph/page.tsx
+++ apps/web/app/(workspace)/workspace/[workspaceId]/note/[noteId]/graph/page.tsx
@@ -1,15 +1,10 @@
 import LocalNoteGraphView from '@notopia-uit/ui/components/local-note-graph-view';
 
-interface GraphPageProps {
-  params: {
-    noteId: string;
-  };
-}
-
-export default function GraphPage({ params }: GraphPageProps) {
+export default async function GraphPage({ params }: { params: Promise<{ noteId: string }> }) {
+  const { noteId } = await params;
   return (
     <div className="h-screen w-full">
-      <LocalNoteGraphView noteId={params.noteId} />
+      <LocalNoteGraphView noteId={noteId} />
     </div>
   );
 }
