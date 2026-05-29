bd36a3f38d556bfe3733cd692086d53c30c89d69
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-05-29 15:55:36 +0700

    fix(notecreateseed): override exist data on seed?

--- internal/notecreateseed/seed.sql
+++ internal/notecreateseed/seed.sql
@@ -1,3 +1,4 @@
 INSERT INTO workspaces (id, slug, name) VALUES
   ('00000000-0000-4000-8000-000000000111', 'ntgnguyen', 'NTNguyen'),
-  ('00000000-0000-4000-8000-000000000112', 'kevinnitro', 'KevinNitro');
+  ('00000000-0000-4000-8000-000000000112', 'kevinnitro', 'KevinNitro')
+ON CONFLICT (id) DO UPDATE SET slug = EXCLUDED.slug, name = EXCLUDED.name;
